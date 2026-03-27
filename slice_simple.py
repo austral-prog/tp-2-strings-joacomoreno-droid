f0c91ffa65c31fe61191e20273606b6e2433665d
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-26 23:05:56 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,3 +3,9 @@ def slice_simple():
     usando slicing y lower().
     """
     texto = "Awesome"
+
+    texto = "Awesome"
+
+    print(texto[:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
