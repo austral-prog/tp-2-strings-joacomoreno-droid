03c9bdbbb56be7e330d2074771128b51aed812ae
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-26 23:03:25 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,3 +3,11 @@ def check_vowels():
     sin distinguir mayúsculas de minúsculas.
     """
     pass
+
+    nombre = input().lower()
+
+    print("Contiene a:", "a" in nombre)
+    print("Contiene e:", "e" in nombre)
+    print("Contiene i:", "i" in nombre)
+    print("Contiene o:", "o" in nombre)
+    print("Contiene u:", "u" in nombre)
